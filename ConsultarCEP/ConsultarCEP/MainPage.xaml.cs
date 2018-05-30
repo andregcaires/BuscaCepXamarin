738c0c83d810562805c1bd511431d39b070de8ba
Andre Caires <andre.caires@trust.com.br>
2018-05-30 16:42:05 -0300

    First

---
+++ ConsultarCEP/ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ConsultarCEP.Servicos;
+using ConsultarCEP.Servicos.Modelos;
+using Newtonsoft.Json;
+
+namespace ConsultarCEP
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            // atribui método ao botão
+            Botao.Clicked += BuscarCep;
+		}
+
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            if (IsValid(Cep.Text.Trim()))
+            {
+                Endereco end = ViaCepService.BuscarEnderecoCep(Cep.Text.Trim());
+                if (end.logradouro != null)
+                {
+                    Resultado.Text = string.Format("Endereco: {0} \nBairro: {1} \nCidade: {2} - {3}", end.logradouro, end.bairro, end.localidade, end.uf);
+                }
+                else
+                {
+                    Resultado.Text = "CEP inválido!";
+                }
+            }
+
+        }
+
+        public bool IsValid(string cep)
+        {
+            if (string.IsNullOrWhiteSpace(cep) || cep.Length != 8 || !int.TryParse(cep, out int cepInt))
+            {
+                DisplayAlert("ERRO", "CEP inválido!", "OK");
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ConsultarCEP/ConsultarCEP/Servicos/ViaCepService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using ConsultarCEP.Servicos.Modelos;
+using Newtonsoft.Json;
+
+namespace ConsultarCEP.Servicos
+{
+    public class ViaCepService
+    {
+        public static string EnderecoUrl = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoCep(string cep)
+        {
+
+            string novo = string.Format(EnderecoUrl, cep);
+
+            WebClient wc = new WebClient();
+            string result = wc.DownloadString(novo);
+
+            return JsonConvert.DeserializeObject<Endereco>(result);
+        }
+    }
+}
